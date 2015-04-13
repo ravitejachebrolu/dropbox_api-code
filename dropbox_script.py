609bf8782e2763dd0d68a9cdf4e5da975eae4263
ravitejachebrolu <rchebrol@hawk.iit.edu>
2015-04-12 20:26:12 -0700

    Dropbox_api

---
+++ dropbox_script.py
@@ -0,0 +1,48 @@
+__author__ = 'raviteja'
+
+# Include the Dropbox SDK libraries
+from dropbox import client, rest, session
+
+# Get your app key and secret from the Dropbox developer website
+
+APP_KEY = 'uj4v8hqnpm3qdsb' #u need to change to your's
+APP_SECRET = 'oh7543hw9mqgkzi' #u need to change to your's
+ACCESS_TYPE = 'dropbox'
+TOKENS = 'dropbox_token.txt'
+
+sess = session.DropboxSession(APP_KEY,APP_SECRET, ACCESS_TYPE )
+
+request_token = sess.obtain_request_token()
+
+# Make the user sign in and authorize this token
+url = sess.build_authorize_url(request_token)
+print "url:", url
+print "Please authorize in the browser. After you're done, press enter."
+raw_input()
+
+# This will fail if the user didn't visit the above URL and hit 'Allow'
+access_token = sess.obtain_access_token(request_token)
+TOKENS = 'dropbox_token.txt'
+token_file = open(TOKENS,'w')
+token_file.write("%s|%s" % (access_token.key,access_token.secret) )
+token_file.close()
+token_file = open(TOKENS)
+token_key,token_secret = token_file.read().split('|')
+token_file.close()
+sess.set_token(token_key,token_secret)
+client = client.DropboxClient(sess)
+#stored_creds = open(CONF_DIR + self.TOKEN_FILE).read()
+print "linked account:", client.account_info()
+#uploading of a file
+f = open('today.txt')
+response = client.put_file('/magnum_opus.txt', f)
+print "uploaded:", response
+#listing of the files
+folder_metadata = client.metadata('/')
+print "metadata:", folder_metadata
+#downloading the files
+f, metadata = client.get_file_and_metadata('/magnum_opus.txt')
+out = open('magnum_opus.txt', 'wb')
+out.write(f.read())
+out.close()
+print metadata
---
+++ readme.txt
@@ -0,0 +1,10 @@
+The dropbox_script.py file contains the code to execute few things:
+1) It is used to upload a file and it can be renamed or can be as it is.
+2) to check what all are present in your dropbox.returns a dictionary of all the available data
+3) to download the files present in the dropbox
+
+
+The program can be modified ,if you want it to be app  specific
+
+1) changing the app permissions to all types and changing the ACCESSTYPE = "dropbox" can help you
+2) to be app specific it should be ACCESSTYPE = "app_folder"
